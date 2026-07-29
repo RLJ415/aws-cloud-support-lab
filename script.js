dd16a17332132e1c519146dc1a2d49c3cbf238fd
Rodney Jones <rodjones415@gmail.com>
2026-07-28 21:25:15 -0400

    Add incident analytics and live search

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
+const searchBox = document.getElementById("searchBox");
+const clearSearch = document.getElementById("clearSearch");
 const incidents = [
     {
-        id: "INC-001",
+        id: "INC-1001",
         service: "Amazon S3",
         severity: "High",
-        status: "Resolved"
+        status: "Open"
     },
     {
-        id: "INC-002",
+        id: "INC-1002",
         service: "Amazon EC2",
         severity: "Medium",
         status: "Investigating"
     },
     {
-        id: "INC-003",
-        service: "IAM",
+        id: "INC-1003",
+        service: "Amazon RDS",
         severity: "Low",
+        status: "Resolved"
+    },
+    {
+        id: "INC-1004",
+        service: "Amazon CloudFront",
+        severity: "Critical",
         status: "Open"
+    },
+    {
+        id: "INC-1005",
+        service: "AWS IAM",
+        severity: "High",
+        status: "Resolved"
     }
 ];
 
@@ -27,9 +41,56 @@ incidents.forEach(function (incident) {
         <tr>
             <td class="incident-id">${incident.id}</td>
             <td>${incident.service}</td>
-            <td>${incident.severity}</td>
-            <td>${incident.status}</td>
+            <td class="${incident.severity.toLowerCase()}">
+    ${incident.severity}
+</td>
+            <td>
+    <span class="status ${incident.status.toLowerCase()}">
+        ${incident.status}
+    </span>
+</td>
         </tr>
     `;
+
+});
+
+document.getElementById("totalIncidents").textContent = incidents.length;
+
+document.getElementById("openIncidents").textContent =
+    incidents.filter(incident => incident.status === "Open").length;
+
+document.getElementById("resolvedIncidents").textContent =
+    incidents.filter(incident => incident.status === "Resolved").length;
+
+document.getElementById("investigatingIncidents").textContent =
+    incidents.filter(incident => incident.status === "Investigating").length;
+
+    searchBox.addEventListener("keyup", function () {
+
+    const searchText = searchBox.value.toLowerCase();
+
+    const rows = document.querySelectorAll("#incidentTable tr");
+
+    rows.forEach(function (row) {
+
+        if (row.textContent.toLowerCase().includes(searchText)) {
+            row.style.display = "";
+        } else {
+            row.style.display = "none";
+        }
+
+    });
+
+});
+
+clearSearch.addEventListener("click", function () {
+
+    searchBox.value = "";
+
+    const rows = document.querySelectorAll("#incidentTable tr");
+
+    rows.forEach(function (row) {
+        row.style.display = "";
+    });
 
 });
